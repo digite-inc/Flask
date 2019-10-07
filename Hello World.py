19883fee2830bba60205e10dae8abb725808c0e1
Suneel Kumar Sowkundi <ssowkundi@digite.com>
2019-10-07 17:36:40 +0530

    Adding file

---
+++ Hello World.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/basic', methods=['POST'])
+def main():
+    word = 'Hello World'
+    return word
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5021, debug=True)
